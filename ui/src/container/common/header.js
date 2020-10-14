b6388ae116ff78088a4035bf934223e192776d9e
raisuladn <raisulru@adndigital.com.bd>
2020-10-14 19:48:33 +0600

    added logout button

--- ui/src/container/common/header.js
+++ ui/src/container/common/header.js
@@ -1,93 +1,115 @@
 import React from 'react';
-import notificationImage from '../../images/notifications.png'
 import userImage from '../../images/user.png'
-import downArray2Image from '../../images/down-arrow-2.png'
 import LogoImage from '../../images/pictogram.svg'
+import keycloak from '../../utils/keycloak'
 
 class Header extends React.Component {
 
   render() {
     return (
-        <div className="menu_area">
-          <div className="container-fluid">
-            <div className="row">
-              <nav className="navbar navbar-expand-lg navbar-light bg-light menu-width">
-                  <a className="navbar-brand" href="#">
-                    <img src={LogoImage}/>
-                  </a>
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <div className="menu_area">
+        <div className="container-fluid">
+          <div className="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light menu-width">
+              <a class="navbar-brand" href="#">
+                <img src={LogoImage} />
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                      <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Contacts
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a className="dropdown-item" href="#">Action</a>
-                        </div>
-                      </li>
-                      <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Marketing
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a className="dropdown-item" href="#">Action</a>
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Contacts
+                </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
 
-                        </div>
-                      </li>
-                      <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Sales
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a className="dropdown-item" href="#">Action</a>
-
-                        </div>
-                      </li>
-                      <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Service
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a className="dropdown-item" href="#">Action</a>
-
-                        </div>
-                      </li>
-                      <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Automation
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a className="dropdown-item" href="#">Action</a>
-
-                        </div>
-                      </li>
-                      <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Reports
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a className="dropdown-item" href="#">Action</a>
-
-                        </div>
-                      </li>
-
-                    </ul>
-                    <div className="form-inline my-2 my-lg-0 icon-menu">
-                      <div className="navbar-nav ml-auto">
-                        <a href="#" className="nav-item nav-link active"><img src={notificationImage} /></a>
-                        <a href="#" className="nav-item nav-link"><img src={userImage} /></a>
-                        <a href="#" className="nav-item nav-link"><img src={downArray2Image} /></a>
-                      </div>
                     </div>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Marketing
+              </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
+
+                    </div>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Sales
+            </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
+
+                    </div>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Service
+          </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
+
+                    </div>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Automation
+              </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
+
+                    </div>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Reports
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
+
+                    </div>
+                  </li>
+
+                </ul>
+                <div class="form-inline my-2 my-lg-0 icon-menu">
+                  <div class="navbar-nav ml-auto">
+                    <li class="nav-item">
+
+                      <a href="#" class="nav-link"><i class="far fa-bell"></i></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <img class="img-profile rounded-circle" src={userImage} />
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Raisul Islam</span>
+                      </a>
+                      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">
+                          <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> Profile
+                        </a>
+                        <a class="dropdown-item" href="#">
+                          <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i> Settings
+                        </a>
+                        <a class="dropdown-item" href="#">
+                          <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i> Activity Log
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal" onClick={() => keycloak.logout()}>
+                          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i> Logout
+                        </a>
+                      </div>
+                    </li>
                   </div>
-              </nav>
-            </div>
-          </div>
+                </div>
+              </div>
+            </nav>
         </div>
+      </div>
+    </div >
     );
   }
 }
