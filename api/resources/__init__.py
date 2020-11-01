17ff95dbd7090681e533869a823b1dc129724420
raisuladn <raisulru@adndigital.com.bd>
2020-11-01 17:51:52 +0600

    Updated mongo uri

--- api/resources/__init__.py
+++ api/resources/__init__.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-mongoclient = MongoClient('mongodb://0.0.0.0:27017/')
+mongoclient = MongoClient('mongodb://localhost:27017/')
 db = mongoclient["lead_generation"]
